8b8c71da8f76d89193c811f5fd696df6c600e1f3
ivanochoa229 <ivanochoa229@gmail.com>
2025-09-27 11:56:19 -0300

    resolviendo pasos repetidos

--- src/vital-signs/vital-signs.module.ts
+++ src/vital-signs/vital-signs.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { VitalSignsService } from './vital-signs.service';
 import { VitalSignsController } from './vital-signs.controller';
+import { PatientsService } from 'src/patients/patients.service';
 
 @Module({
   controllers: [VitalSignsController],
   providers: [VitalSignsService],
+  imports:[PatientsService]
 })
 export class VitalSignsModule {}
